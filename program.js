f65a2c14561314a313e5eb640b02cf1c5784ef3e
Katarzyna Kensy <zbierek@gmail.com>
2018-08-06 14:33:33 +0200

    Add program.js

---
+++ program.js
@@ -0,0 +1,26 @@
+process.stdin.setEncoding('UTF-8');
+
+//process.stdout.write('Write /version for Node version info, /lang for user language info, or /exit to exit program.\n');
+process.stdin.on('readable', function(){
+    var input = process.stdin.read();
+    if(input !== null)
+    {
+      var instruction = input.toString().trim();
+          switch (instruction) {
+              case '/exit':
+                  process.stdout.write('Quitting app!\n');
+                  process.exit();
+                  break;
+              case '/version':
+                  process.stdout.write(process.versions.node + '\n');
+                  break;
+              case '/lang':
+                  process.stdout.write(process.env.lang + '\n');
+                  break;
+              default:
+                  process.stderr.write('Wrong instruction!\n');
+                  break;
+          }
+
+    }
+});
